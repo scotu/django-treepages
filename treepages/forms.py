73565fb788965b384bbe4fd0d10f32a0cce37322
Matteo Scotuzzi <matteo.scotuzzi@gmail.com>
2009-11-29 00:56:09 +0100

    Trying django-tinymce

--- treepages/forms.py
+++ treepages/forms.py
@@ -1,12 +1,12 @@
 from django import forms
 # TODO: remove unused widgets
-from admin_upload.widgets import WYMEditorUpload
+#from admin_upload.widgets import WYMEditorUpload
 #from django_rte_widgets.wmd import WmdTextarea
-#from tinymce.widgets import TinyMCE
+from tinymce.widgets import TinyMCE
 from treepages.models import Page
 
 class PageAdminModelForm(forms.ModelForm):
-    body = forms.CharField(required=False, widget=WYMEditorUpload())
+    body = forms.CharField(required=False, widget=TinyMCE())
 
     class Meta:
         model = Page
